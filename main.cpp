dbf8470e1734cf1f10191ed29e3deabedd5b10e2
LuisFigueroa1 <luisfigueroa8450@gmail.com>
2021-11-26 01:48:34 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//exmaple project using const and it works
+#include "std_lib_facilities.h"
+
+int main()
+{
+    const int a = 5;
+    const int b = 6;
+    const int c = a * b;
+
+    cout<<c;
+}
+
+//it works because the data was never changed
